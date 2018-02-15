166718e0fc0eaf046ffa5bd021fd754c97f6f85e
Umar Farooq <umar.farooq@dfki.de>
2018-02-15 03:34:23 +0100

    Working Steer and Geer shift with Righ Ctrl for upShift and leftCtrl for down Shift

--- CarDriving/Assets/Scripts/DriveAnimControl.cs
+++ CarDriving/Assets/Scripts/DriveAnimControl.cs
@@ -5,18 +5,60 @@ using UnityEngine;
 public class DriveAnimControl : MonoBehaviour {
 
     public GameObject steeringWheel;
-    private float angle;
 
+    private float angle;
     private Quaternion refSteerRotation;
+
+
+    //Root object for Gear lever to rotate the whole lever.
+    public GameObject gearRoot;
+
+    public bool isGearUp, isGearDown,doUp,doDown;
+    private float gearShiftSpeed;
+
+    public DynamicIK dynIk_ref;
 
     // Use this for initialization
     void Start() {
+        gearShiftSpeed = 100f;
         refSteerRotation = steeringWheel.transform.localRotation;
         angle = refSteerRotation.z;
     }
 
     // Update is called once per frame
     void Update() {
+
+        if (isGearUp || Input.GetKey(KeyCode.RightControl))
+        {
+            doUp = true;
+            doDown = false;
+            
+        }
+        else if (isGearDown || Input.GetKey(KeyCode.RightAlt))
+        {
+            doDown = true;
+            doUp = false;
+        }
+
+        if (doUp)
+        {
+            ShiftGearUp();
+            dynIk_ref.ri_targ_ind = 1;
+
+        }
+        else {
+            dynIk_ref.ri_targ_ind = 0;
+        }
+
+        if (doDown)
+        {
+            ShiftGearDown();
+            dynIk_ref.ri_targ_ind = 0;
+            dynIk_ref.ri_targ_ind = 1;
+
+        }
+
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
             SteerRight();
@@ -43,11 +85,26 @@ public class DriveAnimControl : MonoBehaviour {
     void ShiftGearUp()
     {
 
+        //Gear Up shifting Till it reaches the desire rotation level 
+        //As soon as it reaches there make DoUp bool false so it stop 
+        // Calling this functon from update method
+        Debug.Log("Euler Angles "+gearRoot.transform.rotation.z);
+
+        if (gearRoot.transform.rotation.z > -0.13f)
+            gearRoot.transform.Rotate(0f, 0f, -1f * gearShiftSpeed * Time.deltaTime);
+        else
+            doUp = false;
     }
 
     void ShiftGearDown()
     {
-
+        //Doing the Rotation backwards for gear lever. 
+        //If lever reaches to desired rotation make boolean doDown false so 
+        //It doesn't continue more rotation on z-Axis
+        if (gearRoot.transform.rotation.z < 0.13f)
+            gearRoot.transform.Rotate(0f, 0f, 1f * gearShiftSpeed * Time.deltaTime);
+        else
+            doDown = false;
     }
 
 }
--- CarDriving/Assets/Scripts/DynamicIK.cs
+++ CarDriving/Assets/Scripts/DynamicIK.cs
@@ -8,43 +8,56 @@ public class DynamicIK : MonoBehaviour {
     public float horiz_val_debug;
 
     // public and private fields to control the influence of the IK
-    public GameObject ik_target_Le,ik_target_ri;
+    public GameObject[] ik_target_Le;
+    public GameObject[] ik_target_ri;
     public float ikInfluence;
+
+
+    public int ri_targ_ind,le_targ_ind;
     private float ikInfluenceSpeed = 0.5f;
 
-    // Reference to the animator, on wwhich we will set the value of the parameters and the IK info.
-    private Animator anim;
+    public Quaternion rotationOfHand;
 
+    // Reference to the animator, on which we will set the value of the parameters and the IK info.
+    private Animator anim;
     float angle = 2f;
+    
     // Use this for initialization
     void Start () {
+        rotationOfHand = new Quaternion();
+        ri_targ_ind = 0;
+        le_targ_ind = 0;
         this.anim = GetComponent<Animator>();
         InvokeRepeating("RotateRightObj",0f,2f);
     }
 
     // Update is called once per frame
     void Update () {
-		
+	    	
 	}
 
-    void RotateRightObj()
-    {
-        Debug.Log("Calling the func for rotation");
-        ik_target_ri.transform.Rotate(new Vector3(angle + 3f, angle+3f, angle + 3f));
-    }
+    
     void OnAnimatorIK(int layerIndex)
     {
 
         Debug.Log ("ik on layer " + layerIndex);
-        Vector3 ik_target_posle = this.ik_target_Le.transform.position;
-        Vector3 ik_target_posri = this.ik_target_ri.transform.position;
+        Vector3 ik_target_posle = this.ik_target_Le[le_targ_ind].transform.position;
+        Vector3 ik_target_posri = this.ik_target_ri[ri_targ_ind].transform.position;
 
         
         anim.SetIKPosition(AvatarIKGoal.LeftHand, ik_target_posle);
         anim.SetIKPosition(AvatarIKGoal.RightHand, ik_target_posri);
-        anim.SetIKRotationWeight(AvatarIKGoal.RightHand, 1.0F);
 
-        anim.SetIKRotation(AvatarIKGoal.RightHand, Quaternion.LookRotation(ik_target_ri.transform.forward));
+
+        anim.SetIKRotationWeight(AvatarIKGoal.RightHand, 1.0F);
+        Debug.Log("Local Rotation " + ik_target_ri[ri_targ_ind].transform.rotation);
+        anim.SetIKRotation(AvatarIKGoal.RightHand, ik_target_ri[ri_targ_ind].transform.rotation);
+
+
+        anim.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1.0F);
+        Debug.Log("Local Rotation " + ik_target_Le[0].transform.rotation);
+        anim.SetIKRotation(AvatarIKGoal.LeftHand, ik_target_Le[0].transform.rotation);
+
         float delta_ik_influence = this.ikInfluenceSpeed * Time.deltaTime;
 
         this.ikInfluence += delta_ik_influence;
