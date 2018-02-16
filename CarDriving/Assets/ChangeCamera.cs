3b38c3b1775d3e23eb45f1aeafd2e9648edbbcf9
Umar Farooq <umar.farooq@dfki.de>
2018-02-16 12:40:05 +0100

    Final Prototype

---
+++ CarDriving/Assets/ChangeCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour {
+
+    public GameObject[] cameras;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            cameras[0].SetActive(true);
+            cameras[1].SetActive(false);
+            cameras[2].SetActive(false);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            cameras[0].SetActive(false);
+            cameras[1].SetActive(true);
+            cameras[2].SetActive(false);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            cameras[0].SetActive(false);
+            cameras[1].SetActive(false);
+            cameras[2].SetActive(true);
+
+        }
+    }
+}
